20c680f036f85ee76a3184f3a3de9da95d33a4bd
Shikhali Rafiyev <shixeli24@gmail.com>
2016-12-01 14:10:29 +0400

    Working with category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Http\Requests\PostsCreateRequest;
 use App\Photo;
 use App\Post;
@@ -32,9 +33,10 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
+        $categories = Category::lists('name','id')->all();
 
 
-        return view ('admin.posts.create');
+        return view ('admin.posts.create',compact('categories'));
         //
     }
 
